83d65f1cdf8e3b6453c889c087abc78673d872e8
FlutterJarvis <talhaattique777@gmail.com>
2024-12-11 00:33:48 +0500

    12/11/2024

--- lib/smart_tooltip.dart
+++ lib/smart_tooltip.dart
@@ -1,9 +1,4 @@
 library smart_tooltip;
 
-export 'SmartTooltipWithWidget.dart';
-export 'smartTooltip.dart';
-
-library smart_tooltip;
-
 export 'package:smart_tooltip/smartTooltip.dart';
 export 'package:smart_tooltip/smartTooltipWithWidget.dart';
